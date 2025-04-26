ed391e531e5164f5959d6da6155cb87c26c4cb89
Sourav Malo <sourav.cse5.bu@gmail.com>
2025-04-26 11:44:05 +0600

    feat: typescript support removed (closes #12)

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,8 @@
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
-import typescript from 'typescript-eslint';
 
 export default [
     js.configs.recommended,
-    ...typescript.configs.recommended,
     {
         ignores: ['vendor', 'node_modules', 'public', 'tailwind.config.js'],
     },
--- resources/ts/app.ts
+++
@@ -1,8 +0,0 @@
-import './bootstrap';
-
-function add(a: number, b: number): number {
-    const sum: number = a + b;
-    return sum;
-}
-
-console.log(add(2, 3));
